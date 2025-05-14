1108288f55437c540ce1d6d4245986baacdbecd5
DarkCaster <dark.caster@outlook.com>
2025-05-14 19:54:52 +0300

    init.bat and init.sh updated

--- init.bat
+++ init.bat
@@ -11,7 +11,7 @@ if not exist "%venv_dir%" (
 REM You may need to insyall microsoft c++ build tools (from visual studio, may be installed separately), windows 10/11 SDK, CUDA SDK
 
 "%venv_dir%\Scripts\python" -m pip --require-virtualenv install --upgrade pip
-"%venv_dir%\Scripts\python" -m pip --require-virtualenv install --upgrade -r "%script_dir%requirements.txt"
-"%venv_dir%\Scripts\python" -m pip --require-virtualenv install --upgrade torch torchvision torchaudio --index-url=https://download.pytorch.org/whl/cu128
+"%venv_dir%\Scripts\python" -m pip --require-virtualenv install --upgrade torch==2.6.0+cu124 torchvision==0.21.0+cu124 torchaudio==2.6.0+cu124 --index-url="https://download.pytorch.org/whl/cu124"
+"%venv_dir%\Scripts\python" -m pip --require-virtualenv install --upgrade "https://github.com/SystemPanic/vllm-windows/releases/download/v0.8.5/vllm-0.8.5+cu124-cp312-cp312-win_amd64.whl"
 
 endlocal
--- init.sh
+++ init.sh
@@ -6,4 +6,5 @@ if [[ ! -d "$script_dir/venv" ]]; then
   virtualenv "$script_dir/venv"
 fi
 
-"$script_dir/venv/bin/pip" --require-virtualenv install --upgrade -r "$script_dir/requirements.txt"
+# "$script_dir/venv/bin/pip" --require-virtualenv install --upgrade -r "$script_dir/requirements.txt"
+"$script_dir/venv/bin/pip" --require-virtualenv install --upgrade vllm
