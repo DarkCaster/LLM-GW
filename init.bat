b499a67494a98c75772e30704be3768d3f003a2d
DarkCaster <dark.caster@outlook.com>
2025-05-17 04:33:28 +0300

    init.bat updated: added win-requirements.txt for installing vllm

--- init.bat
+++ init.bat
@@ -8,10 +8,11 @@ if not exist "%venv_dir%" (
     python -m venv "%venv_dir%"
 )
 
-REM You may need to insyall microsoft c++ build tools (from visual studio, may be installed separately), windows 10/11 SDK, CUDA SDK
+REM more information for installing vllm on windows:
+REM https://github.com/SystemPanic/vllm-windows
+REM https://github.com/woct0rdho/triton-windows/releases
 
 "%venv_dir%\Scripts\python" -m pip --require-virtualenv install --upgrade pip
-"%venv_dir%\Scripts\python" -m pip --require-virtualenv install --upgrade torch==2.6.0+cu124 torchvision==0.21.0+cu124 torchaudio==2.6.0+cu124 --index-url="https://download.pytorch.org/whl/cu124"
-"%venv_dir%\Scripts\python" -m pip --require-virtualenv install --upgrade "https://github.com/SystemPanic/vllm-windows/releases/download/v0.8.5/vllm-0.8.5+cu124-cp312-cp312-win_amd64.whl"
+"%venv_dir%\Scripts\python" -m pip --require-virtualenv install --upgrade --extra-index-url="https://download.pytorch.org/whl/cu124" -r "%script_dir%win-requirements.txt"
 
 endlocal
