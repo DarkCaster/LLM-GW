7cfa1039bd793a160bf0504b5b64ad870d24d948
DarkCaster <dark.caster@outlook.com>
2026-01-15 19:01:00 +0300

    remove obsolete scripts, create new requirements.txt

--- download_hf_repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-script_dir="$( cd "$( dirname "$0" )" && pwd )"
-
-if [[ ! -d "$script_dir/venv" ]]; then
-  "$script_dir/init.sh"
-fi
-
-"$script_dir/venv/bin/python" "$script_dir/download_hf_repo.py" "$@"
