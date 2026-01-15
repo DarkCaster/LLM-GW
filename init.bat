7ef09e7c2844d8f5d9604761506068802c8af242
DarkCaster <dark.caster@outlook.com>
2026-01-15 20:27:12 +0300

    fixes at init.bat

--- init.bat
+++ init.bat
@@ -2,7 +2,8 @@
 setlocal enabledelayedexpansion
 
 set "script_dir=%~dp0"
-set "base_dir=%CD%"
+set "base_dir=%CD%\"
+REM set "base_dir=%script_dir%"
 
 REM This value will be used with UV to install corresponding python version, may be overriden by params.bat
 set "USE_PYTHON_VER=3.14"
@@ -12,12 +13,12 @@ if exist "%script_dir%params.bat" (
     call "%script_dir%params.bat"
 )
 
-if exist "%base_dir%\params.bat" (
-    echo Loading %base_dir%\params.bat
-    call "%base_dir%\params.bat"
+if exist "%base_dir%params.bat" (
+    echo Loading %base_dir%params.bat
+    call "%base_dir%params.bat"
 )
 
-set "py_dir=%base_dir%\py_dist"
+set "py_dir=%base_dir%py_dist"
 echo Using Python base directory: %py_dir%
 
 echo Installing and upgrading python distribution and venv using UV
@@ -33,7 +34,7 @@ set "UV_LINK_MODE=copy"
 uv python install --upgrade %USE_PYTHON_VER%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-set "venv_dir=%base_dir%\venv"
+set "venv_dir=%base_dir%venv"
 echo Using venv directory: %venv_dir%
 
 if not exist "%venv_dir%" (
