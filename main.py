2fd3e3a55120e1daa1dc72fd82ff1bf9a8d4a350
DarkCaster <dark.caster@outlook.com>
2026-01-15 20:28:31 +0300

    main.py: create main script draft, load lua config

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 import argparse
+import os
 import sys
-from config.config import Config
+import shutil
+from pathlib import Path
+
+# imports from my subpackages
+import config
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Run LLM AWQ models with Huggingface Transformers and serve via Ollama/OpenAI compatible API.",
-        add_help=False
+        description="Run LLM-Gateway, manage LLM engines on demand per request"
     )
-    parser.add_argument(
-        "-h", "--help", action="help", help="Show this help message and exit"
-    )
-    parser.add_argument(
-        "-c", "--config", required=True, help="Path to the YAML config file"
-    )
-
+    parser.add_argument("-c", "--config", required=True, help="Lua configuration file")
     args = parser.parse_args()
 
+    # Load configuration
     try:
-        config = Config(args.config)
-        print("Config loaded successfully.")
-        # TODO: Add further logic for model loading and API serving
+        config_loader = config.ConfigLoader(args.config)
+        cfg = config_loader.cfg
+        temp_dir = config_loader.temp_dir
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"Failed to load configuration: {e}")
+        sys.exit(1)
+
+    print(f"Using temp directory: {temp_dir}")
+    # os.makedirs(temp_dir, exist_ok=True)
+
+    # def temp_cleanup():
+    #    if cfg.get_bool("profile.temp_cleanup", True):
+    #        print(f"[{args.job_id}]: cleaning up temp dir")
+    #        shutil.rmtree(temp_dir, ignore_errors=True)
+
+    try:
+        # Cleanup temporary directory
+        # temp_cleanup()
+        print("Shuting down server")
+
+    except Exception as e:
+        print(f"Unexpected error: {e}")
         sys.exit(1)
 
 
