77b5b993684efa8903bafc9dc5816c66468bfdd1
DarkCaster <dark.caster@outlook.com>
2026-01-15 19:24:25 +0300

    add new init.bat that use uv to manage installation, and init.sh that use system python version

---
+++ params.bat
@@ -0,0 +1,6 @@
+REM UV settings
+set "UV_CONCURRENT_DOWNLOADS=4"
+set "UV_CONCURRENT_INSTALLS=2"
+
+REM proxy server, UV will use it to download packages and python dist
+REM set "ALL_PROXY=socks5://user:password@127.0.0.1:1080"
