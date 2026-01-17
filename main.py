787c9fe3d1753fba223b3c31da54c0a75b2275d3
DarkCaster <dark.caster@outlook.com>
2026-01-17 16:07:09 +0300

    main.py updated

--- main.py
+++ main.py
@@ -20,26 +20,27 @@ def main():
         config_loader = config.ConfigLoader(args.config)
         cfg = config_loader.cfg
         temp_dir = config_loader.temp_dir
+        print(f"Using temp directory: {temp_dir}")
     except Exception as e:
         print(f"Failed to load configuration: {e}")
         sys.exit(1)
 
-    print(f"Using temp directory: {temp_dir}")
-    # os.makedirs(temp_dir, exist_ok=True)
-
-    # def temp_cleanup():
-    #    if cfg.get_bool("profile.temp_cleanup", True):
-    #        print(f"[{args.job_id}]: cleaning up temp dir")
-    #        shutil.rmtree(temp_dir, ignore_errors=True)
+    def temp_cleanup():
+        if cfg.get_bool("profile.temp_cleanup", True):
+            print(f"Cleaning up temp dir {temp_dir}")
+            shutil.rmtree(temp_dir, ignore_errors=True)
 
     try:
-        # Cleanup temporary directory
-        # temp_cleanup()
-        print("Shuting down server")
-
+        print("Starting LLM gateway")
+        # start server here and wait for interrrupt
     except Exception as e:
         print(f"Unexpected error: {e}")
         sys.exit(1)
+    finally:
+        print("Stopping LLM gateway")
+        # add any server termination tasks here
+        temp_cleanup()
+
 
 
 if __name__ == "__main__":
