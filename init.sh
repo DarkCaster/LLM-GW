6c9a2e791b0bdab69ac027da1b90b20951d8c9d7
DarkCaster <dark.caster@outlook.com>
2026-01-18 04:16:52 +0300

    init.sh: minor fixes

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ set -e
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 venv_dir="$script_dir/venv"
-echo Using venv directory: %venv_dir%
+echo "Using venv directory: $venv_dir"
 
 if [[ ! -d "$venv_dir" ]]; then
   python3 -m venv "$venv_dir"
