92dc8c9f8f0463ac54f8d4a8cb7717f640097ffd
DarkCaster <dark.caster@outlook.com>
2026-01-19 23:26:27 +0300

    default params.bat upated

--- params.bat
+++ params.bat
@@ -4,3 +4,6 @@ set "UV_CONCURRENT_INSTALLS=2"
 
 REM proxy server, UV will use it to download packages and python dist
 REM set "ALL_PROXY=socks5://user:password@127.0.0.1:1080"
+
+REM custom directory for pyc files
+REM set "PYTHONPYCACHEPREFIX=%base_dir%cache"
